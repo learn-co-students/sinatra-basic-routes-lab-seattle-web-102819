4a3f5ea77a8d579a9c1e698f867433203d636f0c
moliver4 <tifftang22@gmail.com>
2019-11-18 15:50:57 -0800

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/' do
+        "Hello, World!"
+    end
+
+
+    get '/name' do
+        "My name is Tiffany"
+    end
+
+    get '/hometown' do
+        "My hometown is Davie"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is All Too Well"
+    end
+
 end
