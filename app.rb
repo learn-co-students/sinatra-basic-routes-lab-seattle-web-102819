d3f558b8e7b824871f4f9c1dd6339142701f9115
RobinSanta <robinsntn@gmail.com>
2019-11-18 17:18:39 -0800

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+	
+	get '/' do
+		"Hello, World!"
+	end
+
+	get '/name' do
+		"My name is Robinson Santana Silverio."
+	end
+
+	get '/hometown' do
+		"My hometown is the Bronx."
+	end
+
+	get '/favorite-song' do
+		"My favorite song is all Disturbed songs."
+	end
 end
